b6451d468a0739fdcf702cc81134f9e29bb6acc6
Darius Lupsa <3806325+lupsa@users.noreply.github.com>
2024-05-09 15:10:13 +0300

    Update

---
+++ erp/productie/doctype/bom/test_bom.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Darius and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBOM(FrappeTestCase):
+	pass
--- erp/vanzari/doctype/comenzi/comenzi.py
+++ erp/vanzari/doctype/comenzi/comenzi.py
@@ -2,8 +2,15 @@
 # For license information, please see license.txt
 
 # import frappe
+import datetime
 from frappe.model.document import Document
+from frappe.model.naming import getseries
 
 
 class Comenzi(Document):
-	pass
+    def autoname(self):
+        year = str(datetime.date.today().year)
+        name_prefix = "{}{}".format(year[2:], self.cod_client)
+        seria = getseries(name_prefix, 3)
+        self.name = name_prefix + seria
+
