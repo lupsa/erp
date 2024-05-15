9b627fb0df6b92c596e9ba9aec9b17d91126f78d
Darius Lupsa <3806325+lupsa@users.noreply.github.com>
2024-05-15 11:44:58 +0300

    New update

--- erp/vanzari/doctype/comenzi/comenzi.js
+++ erp/vanzari/doctype/comenzi/comenzi.js
@@ -1,11 +1,25 @@
 // Copyright (c) 2024, Darius and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Comenzi", {
-// 	refresh(frm) {
+frappe.ui.form.on("Comenzi", {
+    onload(frm) {
+        frm.set_query("adresa_de_livrare", function() {
+                return {
+                    "filters": {
+                        denumire_client: frm.doc.client,
+                    }
+                };
+            });
 
-// 	},
-// });
+        frm.set_query("nume_prenume", function() {
+                return {
+                    "filters": {
+                        compania: frm.doc.client,
+                    }
+                };
+            });
+    }
+}),
 
 frappe.ui.form.on('Comenzi lista de produse', {
 	pret_eur(frm, cdt, cdn) {
@@ -21,4 +35,13 @@ frappe.ui.form.on('Comenzi lista de produse', {
         row.total = row.pret_eur * row.cantitate;
         frm.refresh(cdn.total);
 	},
+
+    data_livrare(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        if(row.data_livrare < frm.doc.data_comenzii){
+            row.data_livrare = '';
+            frappe.throw(__('Data de livrare este mai mică decât data comenzii'));
+        }
+    },
+
 })
